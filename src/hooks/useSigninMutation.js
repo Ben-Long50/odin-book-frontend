eeaab2c501bd0bb038c5998a90406a1d53af926a
Ben-Long50 <benjlong50@gmail.com>
2024-12-06 13:09:57 -0800

    added stored query cache clearing on signin

--- src/hooks/useSigninMutation.js
+++ src/hooks/useSigninMutation.js
@@ -1,14 +1,18 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import signin from '../services/signin';
 import { useNavigate } from 'react-router-dom';
 
 const useSigninMutation = (apiUrl, setErrors) => {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   return useMutation({
     mutationFn: async (formData) => {
       return await signin(formData, apiUrl);
     },
-    onSuccess: () => navigate('/home'),
+    onSuccess: () => {
+      queryClient.clear();
+      navigate('/home');
+    },
     onError: (error) => {
       setErrors(error.errors);
     },
